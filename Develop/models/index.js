70fe8b5fba18d9c1e2e18995fb65c7abc0d13116
Plan49 <anthonyplanisek1@gmail.com>
2021-06-22 17:37:52 -0400

    fisned category/tag api routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category)
 // Categories have many Products
-
+Category.hasMany(Product)
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(ProductTag) //ask in class
 // Tags belongToMany Products (through ProductTag)
+TimeRanges.belongsToMany(ProductTag) //ask in class
 
 module.exports = {
   Product,
